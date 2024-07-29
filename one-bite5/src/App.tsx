538e59a44060f4875e5e0b542f2d013913d79690
uuizdashdw <chany920@gmail.com>
2024-07-29 16:09:27 +0900

    [Diary] - 프로젝트 초기화

--- one-bite5/src/App.tsx
+++ one-bite5/src/App.tsx
@@ -1,17 +1,12 @@
 import './App.css'
 
-// Header
-import Header from './components/Header';
 
-// Layout
-import ListLayout from './Layout/ListLayout';
-
-const App = (): JSX.Element => {
+const App = ():JSX.Element => {
   return (
     <div className='App'>
-      <Header />
-      <ListLayout />
+      Diary
     </div>
   )
 }
+
 export default App;
