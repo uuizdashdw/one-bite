85758bc5c59ac42b6503c71d2745bd7908467c43
uuizdashdw <chany920@gmail.com>
2024-07-29 16:30:25 +0900

    [Diary] 1. 라우터 설정 완료 2. Not Found 레이아웃 추가

--- one-bite5/src/App.tsx
+++ one-bite5/src/App.tsx
@@ -1,11 +1,23 @@
 import './App.css'
 
+// Router
+import { Routes, Route } from 'react-router-dom';
+
+// Router Elements
+import HomeLayout from './Layouts/HomeLayout';
+import DiaryLayout from './Layouts/DiaryLayout';
+import NewLayout from './Layouts/NewLayout';
+import NotFoundLayout from './Layouts/NotFoundLayout';
+
 
 const App = ():JSX.Element => {
   return (
-    <div className='App'>
-      Diary
-    </div>
+    <Routes>
+      <Route path='/' element={<HomeLayout />} />
+      <Route path='/diary' element={<DiaryLayout />} />
+      <Route path='/new' element={<NewLayout />} />
+      <Route path='*' element={<NotFoundLayout />} />
+    </Routes>
   )
 }
 
