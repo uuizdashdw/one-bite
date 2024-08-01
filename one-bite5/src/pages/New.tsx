f3d17bf2d2a46939db6487b52403adec0e893929
uuizdashdw <chany920@gmail.com>
2024-08-01 18:48:54 +0900

    [Diary] 1. New.tsx    - 뒤로 가기 버튼 기능 생성 및 적용

--- one-bite5/src/pages/New.tsx
+++ one-bite5/src/pages/New.tsx
@@ -3,11 +3,19 @@ import Header from "../components/common/Header";
 import Button from "../components/Button";
 import Editor from "../components/Editor";
 
+// Navigate Hook
+import { useNavigate } from "react-router-dom";
+
 const New = (): JSX.Element => {
+
+    const navigation = useNavigate();
+
     return (
         <div>
             <Header title="새 일기 쓰기" 
-                    leftChild={<Button text="< 뒤로 가기" type="" onClick={() => console.log()} />} />
+                    leftChild={<Button text="< 뒤로 가기" 
+                                       type="" 
+                                       onClick={() => navigation(-1)} />} />
             <Editor />
         </div>
     )
