f5ea3ddd4a4e6d6c17144bd1c44188bfd2d1fd8a
uuizdashdw <chany920@gmail.com>
2024-08-04 17:05:55 +0900

    [Diary]  1. App.css    - 전체 적용하는 스타일 중 color 속성 제거
     2. Viewer.tsx
       - emotionList 상수 모듈 임포트 및 적용
       - emotionList 의 아이템 타입 임포트 및 적용
       -  기본적인 스타일링을 위한 퍼블리싱
    
     3. viewer.css
       - 스타일링 적용

--- one-bite5/src/components/Viewer.tsx
+++ one-bite5/src/components/Viewer.tsx
@@ -4,20 +4,34 @@ import './../css/viewer.css';
 // Emotion Image Util Function
 import { getEmotionImages } from '../utils/get-emotion-images';
 
+// Emotion List Module
+import { emotionList } from '../utils/constants';
+
+// Types
+import { EmotionItemType } from '../types';
+
 const Viewer = (): JSX.Element => {
 
-    const emotionIdTmp = 1;
+    const emotionIdTmp: number = 1;
+    
+    const emotionItem = emotionList.find((item: EmotionItemType) => item.emotionId === emotionIdTmp) as EmotionItemType;
 
     return (
         <div className='Viewer'>
             <section className='img_section'>
                 <h4>오늘의 감정</h4>
-                <div>
+                <div className={`emotion_img_wrapper emotion_img_wrapper_${emotionIdTmp}`}>
                     <img src={getEmotionImages(emotionIdTmp)} 
                          alt="emotion" />
+                    <div>{emotionItem.emotionName}</div>
                 </div>
             </section>
-            <section className='contenet_section'></section>
+            <section className='contenet_section'>
+                <h4>오늘의 일기</h4>
+                <div className='content_wrapper'>
+                    <p>일기 내용</p>
+                </div>
+            </section>
         </div>
     )
 }
